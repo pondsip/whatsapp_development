246587be9756874c703a4669efafdfec729d00e1
pondsip <93313538+pondsip@users.noreply.github.com>
2021-10-29 11:34:05 +0530

    initial commit

---
+++ Fornested.java
@@ -0,0 +1,14 @@
+class Fornested
+{
+public static void main(String aug[])
+{
+for(int i=5;i<=10;i++)
+{
+ for(int j=5;j<=i;j++) //change the j condition according to the  programs
+{
+  System.out.print("i");
+}
+System.out.println();
+}
+}
+}
